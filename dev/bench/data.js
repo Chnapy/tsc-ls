16be248f839cae5342cdfe7dc8bcba3564b35287
github-action-benchmark <github@users.noreply.github.com>
2022-08-19 22:41:32 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 49e560e38b88eac9c4721deb90fd71acd8ccc02b

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1660948892529,
+  "repoUrl": "https://github.com/Chnapy/tsc-ls",
+  "entries": {
+    "Benchmark.js Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Chnapy",
+            "username": "Chnapy"
+          },
+          "committer": {
+            "name": "Chnapy",
+            "username": "Chnapy"
+          },
+          "id": "49e560e38b88eac9c4721deb90fd71acd8ccc02b",
+          "message": "Add benchmarks",
+          "timestamp": "2022-08-16T08:05:42Z",
+          "url": "https://github.com/Chnapy/tsc-ls/pull/5/commits/49e560e38b88eac9c4721deb90fd71acd8ccc02b"
+        },
+        "date": 1660948891475,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "tsc",
+            "value": 0.16,
+            "range": "±9.25%",
+            "unit": "ops/sec",
+            "extra": "5 samples"
+          },
+          {
+            "name": "tsc-ls",
+            "value": 0.15,
+            "range": "±1.58%",
+            "unit": "ops/sec",
+            "extra": "5 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
