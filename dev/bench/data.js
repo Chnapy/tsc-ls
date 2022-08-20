a505a9426ab26b309c4760a2c70adf17badf850b
github-action-benchmark <github@users.noreply.github.com>
2022-08-20 13:16:15 +0000

    add "tsc -b" vs "tsc-ls -b" no-plugins Benchmark (benchmarkjs) benchmark result for a5b0a27d9d61a60f669845d47e6169b934f81320

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660950440991,
+  "lastUpdate": 1661001375946,
   "repoUrl": "https://github.com/Chnapy/tsc-ls",
   "entries": {
     "Benchmark.js Benchmark": [
@@ -112,6 +112,33 @@ window.BENCHMARK_DATA = {
             "range": "±2.10%",
             "unit": "ops/sec",
             "extra": "5 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Chnapy",
+            "username": "Chnapy"
+          },
+          "committer": {
+            "name": "Chnapy",
+            "username": "Chnapy"
+          },
+          "id": "a5b0a27d9d61a60f669845d47e6169b934f81320",
+          "message": "Improve benchmarks doing comparison",
+          "timestamp": "2022-08-16T08:05:42Z",
+          "url": "https://github.com/Chnapy/tsc-ls/pull/6/commits/a5b0a27d9d61a60f669845d47e6169b934f81320"
+        },
+        "date": 1661001375263,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "performance impact %: \"tsc-ls -b\" vs \"tsc -b\"",
+            "value": 0,
+            "range": "±2.93%",
+            "unit": "%",
+            "extra": "10 samples"
           }
         ]
       }
