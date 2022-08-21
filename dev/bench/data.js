405615e04092a370c6432c511456d52b90d85890
github-action-benchmark <github@users.noreply.github.com>
2022-08-21 15:54:57 +0000

    add "tsc -b" vs "tsc-ls -b" no-plugins Benchmark (benchmarkjs) benchmark result for a7f78048c2e7ee51894a6dc92a77a90a9b9b7027

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1661002521448,
+  "lastUpdate": 1661097297912,
   "repoUrl": "https://github.com/Chnapy/tsc-ls",
   "entries": {
     "\"tsc -b\" vs \"tsc-ls -b\" no-plugins Benchmark": [
@@ -118,6 +118,37 @@ window.BENCHMARK_DATA = {
             "name": "performance impact %: \"tsc-ls -b\" vs \"tsc -b\"",
             "value": 0,
             "range": "±1.53%",
+            "unit": "%",
+            "extra": "10 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "richardhaddad@hotmail.fr",
+            "name": "Richard Haddad",
+            "username": "Chnapy"
+          },
+          "committer": {
+            "email": "richardhaddad@hotmail.fr",
+            "name": "Richard Haddad",
+            "username": "Chnapy"
+          },
+          "distinct": true,
+          "id": "a7f78048c2e7ee51894a6dc92a77a90a9b9b7027",
+          "message": "fix benchmark",
+          "timestamp": "2022-08-21T17:51:12+02:00",
+          "tree_id": "7b792f7dc60a3d31b26a73141f1af8a7c0f1b631",
+          "url": "https://github.com/Chnapy/tsc-ls/commit/a7f78048c2e7ee51894a6dc92a77a90a9b9b7027"
+        },
+        "date": 1661097296932,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "performance impact %: \"tsc-ls -b\" vs \"tsc -b\"",
+            "value": 11.78,
+            "range": "±3.27%",
             "unit": "%",
             "extra": "10 samples"
           }
